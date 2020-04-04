2803e40eb820f2ff061c586b8743d057479241f9
Marcel Zwernemann <marcel.zwernemann@gmail.com>
2020-04-04 13:43:38 +0200

    Now I start to commit

---
+++ components/Component.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include  "./Component.h"
+
+
+using namespace std;
+
+int add(int x, int y) {
+    return x + y;
+}
+
+void printComponent() {
+    cout << "hello from component " << endl;
+}
